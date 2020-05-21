c3e5868a2eb7b3561cb375ff5cc32c91607024e3
unknown <claybeaver1991@gmail.com>
2020-05-21 11:19:18 -0500

    Final Homework submission

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,9 +4,10 @@ import csv
 
 # Variables
 vote_total = 0
-cand_name = ""
-cand_list = []
+cand_dict = {}
 cand_total = 0
+winner_val = 0
+winner = ""
 
 # Import election_data.csv
 # Set path for file
@@ -22,25 +23,42 @@ with open(election_data_path, 'r') as csvfile:
     for row in csvreader:
         # Set row_vote equal to the first vote
         vote_total += 1
-        # Compile the list of candidates
-        if str(row[2]) not in cand_list:
-            cand_list.append(str(row[2]))
-            cand_total += 1
-        # Calculate % of votes each candidate won
-
-
-
-
-
+        #
+        cand_name = str(row[2])
+        # Compile the key value dictionary of candidates and their total votes
+        if cand_name not in cand_dict:
+            cand_dict[cand_name] = 1
+        else: 
+            cand_dict[cand_name] += 1
+            
+# Data Output
 print("Election Results")
 print("------------------------")
 print(f'Total Votes: {vote_total}')
 print("------------------------")
-# for cand_name in cand_list[0]:
-#     print(cand_name,end='')
-# for cand_name in cand_list[1]:
-#     print(cand_name,end='')
-# for cand_name in cand_list[2]:
-#     print(cand_name,end='')
-# for cand_name in cand_list[3]:
-#     print(cand_name,end='')
+# For loop to add Candidate Percentage and Winner into output
+for key,val in cand_dict.items():
+    cand_perc = round((val / vote_total) * 100, 2)
+    print(f'{key}: %{cand_perc:.3f} ({val})')
+    if cand_perc > winner_val:
+        winner_val = cand_perc
+        winner = key    
+print("------------------------")
+print(f'Winner: {winner}')
+print("------------------------")
+
+#Data Output into Text File
+with open("OutputAnalysis2.txt", "a") as e:
+    print("Election Results", file = e)
+    print("------------------------", file = e)
+    print(f'Total Votes: {vote_total}', file = e)
+    print("------------------------", file = e)
+    for key,val in cand_dict.items():
+        cand_perc = round((val / vote_total) * 100, 2)
+        print(f'{key}: %{cand_perc:.3f} ({val})', file = e)
+        if cand_perc > winner_val:
+            winner_val = cand_perc
+            winner = key    
+    print("------------------------", file = e)
+    print(f'Winner: {winner}', file = e)
+    print("------------------------", file = e)
